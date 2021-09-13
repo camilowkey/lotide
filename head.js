b35031f5b39b2b357e7a13c9442d8e98ff859dcc
Cj <john.camilo.mades@gmail.com>
2021-09-13 23:05:41 +0000

    updates with lint and tail function

--- head.js
+++ head.js
@@ -16,7 +16,7 @@ const head = function(array) {
     console.log(array[0]);
   }
   return array[0];
-}
+};
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
