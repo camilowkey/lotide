8ff793e8b0e912ca365fa705b7043cfa971bf9c6
Cj <john.camilo.mades@gmail.com>
2021-09-13 21:34:24 +0000

    this has emojis in it to tell us whether or not its true or false

---
+++ assertEqual.js
@@ -0,0 +1,17 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } if (actual !== expected) {
+    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("yes", "yes");
+assertEqual("yes", "no");
+assertEqual(777, 777);
+assertEqual(7, 77);
